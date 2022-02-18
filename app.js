dba84de06d760b49c6bb4e835f8031af7b6fbfc6
Toni Gashi <toni.gashi@sciant.com>
2022-02-18 13:42:09 +0200

    Task02 completed and rebased

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
 require('dotenv').config()
-const { Pool } = require('pg')
+const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const { authenticateJWT } = require('./middleware/authJWT')
+const { dbConnector } = require('./middleware/dbConnect');
+const port = 3000;
+const app = express();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-})
+app.use(bodyParser.json());
+app.use(express.json())
+app.use(dbConnector);
+
+try {
+  checkRequirements();
+} catch (err) {
+  console.log(err.message);
+  return;
+}
+
+
+app.post('/user', authenticateJWT, async function(req, res) {
+  try {
+    const { email, password } = req.body;
+    await createUser(email, password, req.pool);
+    return res.send({
+      status: 200,
+      message: 'User created successfully'
+    })
+  } catch (err) {
+    console.log('[ERROR]: ', err.message);
+    return res.status(err.statusCode || 500).send({
+      error: { message: err.message },
+    });
+  }
+});
+
+app.post('/login', async function(req, res) {
+  const { email, password } = req.body;
+  try {
+    await loginUser(email, password, req.pool)
+    const accessToken = jwt.sign({ username: email }, process.env.JWT_SECRET_TOKEN);
+    console.log('[DEBUG]: Access token generated');
+    process.env.JWT_ACCESS_TOKEN = accessToken;
+    return res.send({
+      status: 200,
+      message: 'User login successfully',
+      accessToken
+    })
+  } catch (err) {
+    console.log('[ERROR]: ', err.message);
+    return res.status(err.statusCode || 500).send({
+      error: { message: err.message },
+    });
+  }
 
 function checkRequirements() {
   let errMessage = '';
@@ -28,15 +73,25 @@ function checkRequirements() {
   if(!process.env.DB_DATABASE) {
     errMessage += "Missing DB_DATABASE in the .env file\n"
   }
+  if(!process.env.JWT_SECRET_TOKEN) {
+    errMessage += "Missing JWT_SECRET_TOKEN in the .env file\n"
+  }
+  if(!process.env.BCRYPT_ROUNDS) {
+    errMessage += "Missing BCRYPT_ROUNDS in the .env file\n"
+  }
 
-  return errMessage;
+  if(errMessage) {
+    throw new Error(errMessage);
+  }
 }
 
 function isValidEmail(email) {
   if(String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
     return true;
   } else {
-    return false;
+    const error =  new Error('Invalid email');
+    error.statusCode = 400;
+    throw error;
   }
 }
 
@@ -44,7 +99,9 @@ function isValidPassword(pass) {
   if(String(pass).toLowerCase().match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/)) {
     return true;
   } else {
-    return false;
+    const error =  new Error('Invalid password');
+    error.statusCode = 400;
+    throw error;
   }
 }
 
@@ -70,16 +127,17 @@ async function createUser(email, password) {
   await pool.connect();
 
   console.log('[DEBUG]: Hashing the password');
-  const hash = await bcrypt.hash(password, 10)
+  const hash = await bcrypt.hash(password, parseInt(process.env.BCRYPT_ROUNDS))
 
   console.log('[DEBUG]: Sending the request to database');
   try {
-    await newQuery(`INSERT INTO users (email, dpassword) VALUES ($1, $2)`, [email, hash], pool);
+    await newQuery(`INSERT INTO users (email, dhash) VALUES ($1, $2)`, [email, hash], pool);
     console.log('[DEBUG]: User created successfully');
-    process.exit(1);
+    return;
   } catch (err) {
-    console.log(err.message);
-    process.exit(1);
+    const error =  new Error('Email already exists')
+    error.statusCode = 400
+    throw error;
   }
 }
 
@@ -96,11 +154,6 @@ async function loginUser(email, password) {
     return 'Wrong email format';
   }
   console.log('[DEBUG]: Email validated');
-
-  if(!isValidPassword(password)) {
-    return 'Wrong password format';
-  }
-  console.log('[DEBUG]: Password validated');
 
   await pool.connect();
 
@@ -122,8 +175,9 @@ async function loginUser(email, password) {
       throw new Error('User not found');
     }
   } catch (err) {
-    console.log(err.message);
-    process.exit(1);
+    const error =  new Error(err.message);
+    error.statusCode = 404;
+    throw error;
   }
 }
 
@@ -133,12 +187,5 @@ async function newQuery(query, values, pool) {
       text: query,
       values
   });
-  return result;
+  return result.rows
 }
-
-module.exports = {
-  createUser,
-  loginUser
-}
-
-require('make-runnable')
